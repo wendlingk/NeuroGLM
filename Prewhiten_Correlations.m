6cb0fec4707ab254e34c516c97ac63c62fec117c
wendlingk <44070884+wendlingk@users.noreply.github.com>
2020-08-06 13:24:59 -0400

    Update Prewhiten_Correlations.m

--- Prewhiten_Correlations.m
+++ Prewhiten_Correlations.m
@@ -2,59 +2,59 @@
 % Otherwise, load initial Decoding Stats structure and skip to next section
 
 load Decoding_Stats_Range500.mat %1x6 struct, netSizes=2,..,7
-ErrStat=Error_Stats;
-nmNtSz=size(Error_Stats,2); %6 total network sizes, 2,..,7
+DecodeStat=Decoding_Stats;
+nmNtSz=size(Decoding_Stats,2); %6 total network sizes, 2,..,7
 load Decoding_Stats_Range1000.mat
 for j=1:nmNtSz
-    Error_Stats(j).Range=[Error_Stats(j).Range ErrStat(j).Range];
-    Error_Stats(j).Error=[Error_Stats(j).Error ErrStat(j).Error];
-    Error_Stats(j).Bias_diff=[Error_Stats(j).Bias_diff ErrStat(j).Bias_diff];
-    Error_Stats(j).Stim_diff=[Error_Stats(j).Stim_diff ErrStat(j).Stim_diff];
-    Error_Stats(j).Post_diff=[Error_Stats(j).Post_diff ErrStat(j).Post_diff];
-    Error_Stats(j).Stimulus=[Error_Stats(j).Stimulus ErrStat(j).Stimulus];
-    Error_Stats(j).Optimal=[Error_Stats(j).Optimal ErrStat(j).Optimal];
-    Error_Stats(j).CholeskyFlag=[Error_Stats(j).CholeskyFlag Err_Stats(j).CholeskyFlag];
+    Decoding_Stats(j).Range=[Decoding_Stats(j).Range DecodeStat(j).Range];
+    Decoding_Stats(j).Error=[Decoding_Stats(j).Error DecodeStat(j).Error];
+    Decoding_Stats(j).Bias_diff=[Decoding_Stats(j).Bias_diff DecodeStat(j).Bias_diff];
+    Decoding_Stats(j).Stim_diff=[Decoding_Stats(j).Stim_diff DecodeStat(j).Stim_diff];
+    Decoding_Stats(j).Post_diff=[Decoding_Stats(j).Post_diff DecodeStat(j).Post_diff];
+    Decoding_Stats(j).Stimulus=[Decoding_Stats(j).Stimulus DecodeStat(j).Stimulus];
+    Decoding_Stats(j).Optimal=[Decoding_Stats(j).Optimal DecodeStat(j).Optimal];
+    Decoding_Stats(j).CholeskyFlag=[Decoding_Stats(j).CholeskyFlag DecodeStat(j).CholeskyFlag];
 end
-%repeat
-ErrStat=Error_Stats;
+% Repeat for as all MAT files being combined (in this case, two loops for three MAT files)
+DecodeStat=Decoding_Stats;
 load Decoding_Stats_Range2000.mat
 for j=1:nmNtSz
-    Error_Stats(j).Range=[Error_Stats(j).Range ErrStat(j).Range];
-    Error_Stats(j).Error=[Error_Stats(j).Error ErrStat(j).Error];
-    Error_Stats(j).Bias_diff=[Error_Stats(j).Bias_diff ErrStat(j).Bias_diff];
-    Error_Stats(j).Stim_diff=[Error_Stats(j).Stim_diff ErrStat(j).Stim_diff];
-    Error_Stats(j).Post_diff=[Error_Stats(j).Post_diff ErrStat(j).Post_diff];
-    Error_Stats(j).Stimulus=[Error_Stats(j).Stimulus ErrStat(j).Stimulus];
-    Error_Stats(j).Optimal=[Error_Stats(j).Optimal ErrStat(j).Optimal];
-    Error_Stats(j).CholeskyFlag=[Error_Stats(j).CholeskyFlag Err_Stats(j).CholeskyFlag];
+    Decoding_Stats(j).Range=[Decoding_Stats(j).Range DecodeStat(j).Range];
+    Decoding_Stats(j).Error=[Decoding_Stats(j).Error DecodeStat(j).Error];
+    Decoding_Stats(j).Bias_diff=[Decoding_Stats(j).Bias_diff DecodeStat(j).Bias_diff];
+    Decoding_Stats(j).Stim_diff=[Decoding_Stats(j).Stim_diff DecodeStat(j).Stim_diff];
+    Decoding_Stats(j).Post_diff=[Decoding_Stats(j).Post_diff DecodeStat(j).Post_diff];
+    Decoding_Stats(j).Stimulus=[Decoding_Stats(j).Stimulus DecodeStat(j).Stimulus];
+    Decoding_Stats(j).Optimal=[Decoding_Stats(j).Optimal DecodeStat(j).Optimal];
+    Decoding_Stats(j).CholeskyFlag=[Decoding_Stats(j).CholeskyFlag DecodeStat(j).CholeskyFlag];
 end
 
-%% Assess stationarity via Adjusted Dickey-Fuller and KPSS Tests
+%% Assess stationarity via Augmented Dickey-Fuller and KPSS Tests
 
 % Original time series (Actual stimulus and decoded stimulus)
-adfhyp_x = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-adfhyp_y = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-kpsshyp_x = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-kpsshyp_y = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-for i = 1:length(Error_Stats)
-    x = Error_Stats(i).Stimulus;
-    y = Error_Stats(i).Optimal;    
+adfhyp_x = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+adfhyp_y = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+kpsshyp_x = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+kpsshyp_y = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+for i = 1:length(Decoding_Stats)
+    x = Decoding_Stats(i).Stimulus;
+    y = Decoding_Stats(i).Optimal;    
     for j=1:size(x,2)
-        adfhyp_x(j,i) = adftest(x(:,j),'Alpha',0.01);
-        adfhyp_y(j,i) = adftest(y(:,j),'Alpha',0.01);
+        adfhyp_x(j,i) = adftest(x(:,j),'Alpha',0.001);
+        adfhyp_y(j,i) = adftest(y(:,j),'Alpha',0.001);
         kpsshyp_x(j,i) = kpsstest(x(:,j),'Alpha',0.1);
         kpsshyp_y(j,i) = kpsstest(y(:,j),'Alpha',0.1);
     end
 end
 
 % Time series differenced once
-adfhyp_xdiff = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-adfhyp_ydiff = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-kpsshyp_xdiff = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-kpsshyp_ydiff = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-for i = 1:length(Error_Stats)
-    x = diff(Error_Stats(i).Stimulus);
-    y = diff(Error_Stats(i).Optimal);    
+adfhyp_xdiff = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+adfhyp_ydiff = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+kpsshyp_xdiff = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+kpsshyp_ydiff = zeros(size(Decoding_Stats(1).Stimulus,2),lengthDecoding_Stats));
+for i = 1:length(Decoding_Stats)
+    x = diff(Decoding_Stats(i).Stimulus);
+    y = diff(Decoding_Stats(i).Optimal);    
     for j=1:size(x,2)
         adfhyp_xdiff(j,i) = adftest(x(:,j),'Alpha',0.01);
         adfhyp_ydiff(j,i) = adftest(y(:,j),'Alpha',0.01);
@@ -67,13 +67,13 @@ end
 
 % Try-catch needed for large number of iterations for when original
 % parameters produce a matrix that is unstable or not invertible
-corradj = zeros(size(Error_Stats(1).Stimulus,2),length(Error_Stats));
-for i = 1:length(Error_Stats)
+corradj = zeros(size(Decoding_Stats(1).Stimulus,2),length(Decoding_Stats));
+for i = 1:length(Decoding_Stats)
     % Differenced time series chosen here for more reliable stationarity
     % Thus, all ARIMA models below actually are ARIMA(p,1,q)
-    x = diff(Error_Stats(i).Stimulus); 
-    y = diff(Error_Stats(i).Optimal);
-    for j=1:size(Error_Stats(i).Stimulus,2)
+    x = diff(Decoding_Stats(i).Stimulus); 
+    y = diff(Decoding_Stats(i).Optimal);
+    for j=1:size(Decoding_Stats(i).Stimulus,2)
         try
             try
                 xMdl = estimate(arima(6,0,3),x(:,j)); % ARIMA(6,1,3)
